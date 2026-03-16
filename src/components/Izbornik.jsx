2a2470b2e0270e18e46aa0f25b501827a8c531b8
marija dergez <45633765+marijadergez@users.noreply.github.com>
2026-03-16 15:09:23 +0100

    npm run build

--- src/components/Izbornik.jsx
+++ src/components/Izbornik.jsx
@@ -28,6 +28,7 @@ export default function Izbornik() {
                         
                         <NavDropdown title="Ponuda" id="basic-nav-dropdown">
                             <NavDropdown.Item 
+                            
                             onClick={()=>navigate(RouteNames.USLUGE)}
                             
                             >Usluge</NavDropdown.Item>
@@ -37,7 +38,10 @@ export default function Izbornik() {
                             
                             onClick={()=>navigate(RouteNames.KONTAKT)}
                             >Kontakt</NavDropdown.Item>
+
+
                             <NavDropdown.Divider />
+
                             <NavDropdown.Item href="#action/3.4">
                                 Separated link
                             </NavDropdown.Item>
