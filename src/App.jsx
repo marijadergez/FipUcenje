9dd57eca3a10d4c5658c79653d78e3dcfd9ef4c3
marija dergez <45633765+marijadergez@users.noreply.github.com>
2026-03-16 14:05:16 +0100

    route ne rade

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Container } from 'react-bootstrap'
 import Izbornik from './components/Izbornik'
 import { Route, Routes } from 'react-router-dom'
 import { RouteNames } from './constants'
+import UslugePregled from './usluge/UslugePregled'
 
 function App() {
 
@@ -11,7 +12,7 @@ function App() {
     <Container>
       <Izbornik />
       <Routes>
-      <Route path={RouteNames.HOME} />
+        
       </Routes>
       <hr />
 
